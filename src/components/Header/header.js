18729771141dd7f1fea087129124c01d27b25461
Julio Pampuch <juliopampuch@gmail.com>
2022-12-01 20:52:18 -0300

    first commit

---
+++ src/components/Header/header.js
@@ -0,0 +1,11 @@
+import "./header.css"
+
+const Header = () => {
+  return (
+      <header className="banner">
+        <img src="/imagens/banner.png" alt="banner organo" />
+      </header>
+  )
+}
+
+export default Header
