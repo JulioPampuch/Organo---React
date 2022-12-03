0954960b271f8d1f900b5e615dc6e391b9e4ba05
Julio Pampuch <juliopampuch@gmail.com>
2022-12-03 00:55:45 -0300

    create select input

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,7 +1,20 @@
+import Dropdown from "../DropdownList"
 import TextArea from "../TextArea"
 import "./form.css"
 
 const Form = () => {
+
+  const times = [
+    "Programação",
+    "Front-end",
+    "Data-Science",
+    "DevOps",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão"
+  ]
+
+
   return (
     <section className="section-form">
       <h2>Preencha os dados para criar o card do colaborador.</h2>
@@ -9,6 +22,7 @@ const Form = () => {
       <TextArea name="Nome" placeholder="Digite seu nome" />
       <TextArea name="Cargo" placeholder="Digite seu cargo" />
       <TextArea name="Imagem" placeholder="Informe o endereço da imagem" />
+      <Dropdown label="Time" itens={times} />
       </form>
     </section>
   )
