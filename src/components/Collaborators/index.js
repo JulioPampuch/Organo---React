f321df84212f9e1eefc4c98601343944375782c1
Julio Pampuch <juliopampuch@gmail.com>
2022-12-06 01:23:17 -0300

    styled cards components

---
+++ src/components/Collaborators/index.js
@@ -0,0 +1,20 @@
+import "./collaborators.css"
+
+const Collaborator = () => {
+  return (
+    <div className="collaborator">
+        <div className="header">
+          <div className="img">
+            
+          </div>
+          {/* <img src="https://github.com/JulioPampuch.png" alt="Julio Pampuch" /> */}
+      </div>
+      <div className="footer">
+        <h4>Julio Pampuch</h4>
+        <h5>Desenvolvedor de software e instrutora</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Collaborator
--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -1,3 +1,4 @@
+import Collaborator from "../Collaborators"
 import "./team.css"
 
 const Team = (props) => {
@@ -5,6 +6,8 @@ const Team = (props) => {
   return (
     <section className="team" style={{ backgroundColor: props.secundaryColor }}>
       <h3 style={{ borderColor: props.primaryColor }}>{props.name}</h3>
+      <Collaborator />
+      <Collaborator />
     </section>
   )
 }
